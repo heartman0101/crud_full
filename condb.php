fa48c63f5b420e7d8c40dd37926ab7227e0abd4d
Vachiravich <157151124+heartman0101@users.noreply.github.com>
2026-05-06 12:29:03 +0700

    Update condb.php

--- condb.php
+++ condb.php
@@ -2,11 +2,7 @@
 
 $host = "localhost";
 $username = "root";
-<<<<<<< HEAD
-$password = "";
-=======
 $password = "65500706960";
->>>>>>> 6db8e2594f25dd62f7e2e8ad32e36112b14b71d8
 $dbname = "crud_task";
 
 try {
